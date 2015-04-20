6035399e4179e56e3dcc0029ec6aa4585e0c18d7
Erik van Dam <erikvandam@eriktisme.nl>
2015-04-20 11:41:30 +0200

    Add main

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+
+/**
+ *
+ * @author ppthgast
+ */
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //
+    }
+}
